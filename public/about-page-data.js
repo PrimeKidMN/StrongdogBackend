e0a5d036562a281bbca0dc691c3ae6beb7e26d05
Proxations <135172681+PrimeKidMN@users.noreply.github.com>
2025-03-01 11:35:00 -0500

    Update and rename about-page-data.ts to about-page-data.js

--- public/about-page-data.js
+++ public/about-page-data.js
@@ -1,4 +1,5 @@
-export const updates: { date: string; content: string }[] = [
+// All of the updates and their corresponding dates that will be dynamically added in the updates section on about page 
+export const updates = [
 	{ date: "2/24/25", content: "Added a card size slider to the main menu" },
 	{ date: "2/10/25", content: "Added block lanschool 3.0 to secret menu" },
 	{ date: "11/18/24", content: "Added stickman boost, stickman boost 2, stick duel battle, stick archers battle, starblast.io, sprinter, soccer skills euro cup, snow battle.io, sniper shot, snay.io, small world cup, and tennis random to the game testing page" },
@@ -32,7 +33,8 @@ export const updates: { date: string; content: string }[] = [
 	{ date: "5/12/24", content: "Added About section" },
 ];
 
-export const modes: { mode: string; properties: { [key: string]: string } }[] = [
+// This holds all of the color themes for light/dark mode
+export const modes = [
 	{
 		mode: "light",
 		properties: {
