9703b84d1a6d5644b2e72349f537358fb153055c
Proxations <135172681+PrimeKidMN@users.noreply.github.com>
2025-03-03 11:19:44 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the 'public' directory at the root level
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Catch-all route to serve the index.html file for all requests
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-const port = Number(process.env.PORT) || 3000;
+// Default to PORT 3000, but you can use a different port if needed
+const port = process.env.PORT || 3000; // You can change 3000 to another port if it's occupied
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
